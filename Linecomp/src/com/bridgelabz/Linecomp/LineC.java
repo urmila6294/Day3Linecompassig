1130cf0e4288e980cf17b36a6d1cb3a66fdc8a2b
urmila4462 <dasari.urmila4462@gmail.com>
2022-05-04 19:43:13 +0530

    Added diaplay message

---
+++ Linecomp/src/com/bridgelabz/Linecomp/LineC.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.Linecomp;
+
+public class LineC {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Line Comparison Computation");
+	}
+
+}
